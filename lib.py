381966a1f90ca0874388dba27270086448e934b8
unknown <krunalgadher250@gmail.com>
2025-03-18 18:21:29 +0530

    first commit

--- lib.py
+++ lib.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,Path,HTTPException
 from pydantic import BaseModel
 from pymongo import MongoClient
+from bson import ObjectId
 
 app = FastAPI()
 
@@ -39,3 +40,23 @@ async def get_books(db: MongoClient = Depends(get_current_database)):
         books['_id'] = str(books['_id'])
         book_list.append(books)
     return book_list
+
+@app.get("/Read_book/{book_id}")
+async def get(book_id: str = Path(...),db: MongoClient = Depends(get_current_database)):
+    book = users_collection.find_one({"_id": ObjectId(book_id)})
+    if book:
+        book["_id"]= str(book["_id"])
+        return book
+    else:
+        raise HTTPException (status_code=404,detail = "user not found")
+
+@app.put("/Read_book/{book_id}")
+async def update_book(book_data:Books,book_id:str = Path(...),db: MongoClient = Depends(get_current_database)):
+    book_dict = book_data.model_dump()
+    book_obj_id = ObjectId(book_id)
+    result = users_collection.update_one({"_id":book_obj_id},{'$set':book_dict})
+    if result.modified_count ==1:
+        return {"Book updated successfully"}
+    else:
+        raise HTTPException(status_code=404,detail="book not found")
+    
